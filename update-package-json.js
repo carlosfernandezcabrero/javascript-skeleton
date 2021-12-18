d2237eb08a177191dd5e45c8768ce56636711172
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2021-12-18 11:56:30 +0100

    feat: :sparkles: Added option for change name of author

--- update-package-json.js
+++ update-package-json.js
@@ -10,19 +10,23 @@ const rl = createInterface({
 (() => {
     rl.question("Project name: ", (project_name) => {
         rl.question("Project description (short): ", (project_description) => {
-            project_name = project_name.trim();
-            project_description = project_description.trim();
+            rl.question("Project author: ", (project_author) => {
+                project_name = project_name.trim();
+                project_description = project_description.trim();
+                project_author = project_author.trim();
 
-            if (project_name.length > 0) data.name = project_name;
-            if (project_description.length > 0)
-                data.description = project_description;
+                if (project_name.length > 0) data.name = project_name;
+                if (project_description.length > 0)
+                    data.description = project_description;
+                if (project_author.length > 0) data.author = project_author;
 
-            writeFileSync(
-                "./package.json",
-                `${JSON.stringify(data, null, 4)}\n`
-            );
+                writeFileSync(
+                    "./package.json",
+                    `${JSON.stringify(data, null, 4)}\n`
+                );
 
-            rl.close();
+                rl.close();
+            });
         });
     });
 })();
